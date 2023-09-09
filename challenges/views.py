ed1bff399f9d2d23b2d17079be46a293cfcfc4b1
mohammadT9030 <torabi9030@gmail.com>
2023-09-09 12:39:45 +0330

    Implementing int url for months

--- challenges/views.py
+++ challenges/views.py
@@ -6,8 +6,17 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 challenges = {"january": "Exercise 30 min every other day!"}
 
+months_list = ["january"]
+
 
 def monthly_challenge(request, month):
     if not month in challenges:
         return HttpResponseNotFound("this month is not supported!")
     return HttpResponse(challenges[month])
+
+
+def monthly_challenge_by_number(request, month):
+    if month > len(months_list):
+        return HttpResponseNotFound("this month is not supported!")
+
+    return monthly_challenge(request, months_list[month - 1])
