2dac6f07c6a0a53df9bd8175f55de1b7459aca5b
mohammadT9030 <torabi9030@gmail.com>
2023-09-13 11:47:35 +0330

    Adding navigation bar using include tag (DTL)

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.list_of_months),
+    path("", views.list_of_months, name="all-challenges"),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge"),
 ]
